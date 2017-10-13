58e2abddbd5d42fa046cc5f1be76989245b24c51
Matthieu Marais <mqtthieu@gmail.com>
2017-10-13 10:42:41 +0200

    input search

--- pokedex/src/app/services/pokemon-api.service.ts
+++ pokedex/src/app/services/pokemon-api.service.ts
@@ -8,11 +8,12 @@ export class PokemonApiService {
   constructor(private http: HttpClient) { }
 
   getAllPokemons(pageNumber: number) {
-    return this.http.get('http://pokeapi.salestock.net/api/v2/pokemon' + "?offset=" + ((pageNumber - 1) * 20));
+    return this.http.get('https://pokeapi.co/api/v2/pokemon' + "?offset=" + ((pageNumber - 1) * 20));
   }
 
   getDetailPokemon(name: string){
-    return this.http.get('http://pokeapi.salestock.net/api/v2/pokemon/'+ name);
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/'+ name);
   }
  
 }
+//http://pokeapi.salestock.net/api/v2/pokemon
